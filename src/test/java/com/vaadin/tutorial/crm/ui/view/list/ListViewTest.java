10bb8db1d0ed0690df12361fc4757252fb577bd1
gh1s <ghislingrandbois@gmail.com>
2021-03-14 21:39:54 +1100

    Initial commit

---
+++ src/test/java/com/vaadin/tutorial/crm/ui/view/list/ListViewTest.java
@@ -0,0 +1,20 @@
+package com.vaadin.tutorial.crm.ui.view.list;
+
+import com.vaadin.tutorial.crm.ui.view.list.ListView;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListViewTest {
+
+    @Autowired
+    private ListView listView;
+
+    @Test
+    public void formShownWhenContactSelected() {
+    }
+}
